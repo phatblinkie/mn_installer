8e7a1f7af79d6bf181d34a95eacabd86cab3c3e1
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-09-06 17:20:21 -0600

    Parse log for good things

--- log_parser/pirlnode_log_parser.py
+++ log_parser/pirlnode_log_parser.py
@@ -2,10 +2,29 @@
 
 from systemd import journal
 
-j = journal.Reader()
-j.this_boot()
-j.log_level(journal.LOG_INFO)
-j.add_match(_SYSTEMD_UNIT="pirlnode.service")
+def main():
+    j = journal.Reader()
+    j.this_boot()
+    j.log_level(journal.LOG_INFO)
+    j.add_match(_SYSTEMD_UNIT="pirlnode.service")
 
-for entry in j:
-    print(entry['MESSAGE'])
+    # Look for these key words or phrases
+    onetime_keyitems = ['UDP listener up', 'RLPx listener up', 'IPC endpoint opened']
+    importing_blocks = 'Imported new chain segment'
+    importing_blocks_check = 0
+    sending_proof = ' masternode sending proof of activity for block'
+    sending_proof_check = 0
+    for entry in j:
+        if onetime_keyitems in entry['MESSAGE']:
+            print(f"This is good --> {entry['MESSAGE']}")
+
+        if importing_blocks in entry['MESSAGE'] and importing_blocks_check == 0:
+            importing_blocks_check = 1
+            print(f"Importing blocks is good --> {entry['MESSAGE']} ")
+
+        if sending_proof in entry['MESSAGE'] and sending_proof_check == 0:
+            sending_proof_check = 1
+            print(f"Sending proof is good --> {entry['MESSAGE']} ")
+
+if __name__ == "__main__":
+    main()
