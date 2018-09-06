dd32d323e615310f11bc7785e42801420875875b
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2018-09-06 17:22:12 -0600

    can't use f-strings in python2

--- log_parser/pirlnode_log_parser.py
+++ log_parser/pirlnode_log_parser.py
@@ -16,15 +16,15 @@ def main():
     sending_proof_check = 0
     for entry in j:
         if onetime_keyitems in entry['MESSAGE']:
-            print(f"This is good --> {entry['MESSAGE']}")
+            print("This is good --> {}".format(entry['MESSAGE']))
 
         if importing_blocks in entry['MESSAGE'] and importing_blocks_check == 0:
             importing_blocks_check = 1
-            print(f"Importing blocks is good --> {entry['MESSAGE']} ")
+            print("Importing blocks is good --> {}".format(entry['MESSAGE']))
 
         if sending_proof in entry['MESSAGE'] and sending_proof_check == 0:
             sending_proof_check = 1
-            print(f"Sending proof is good --> {entry['MESSAGE']} ")
+            print("Sending proof is good --> {}".format(entry['MESSAGE']))
 
 if __name__ == "__main__":
     main()
