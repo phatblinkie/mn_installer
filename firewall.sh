27ba1e305c4a9fe0bd700c841fe201cdd87c3f6e
plataplomo <43726571+plataplomo@users.noreply.github.com>
2018-09-30 16:17:58 -0500

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SECTION_SEPARATOR="========================================="
+
 function validateIP()
  {
          local ip=$1
@@ -15,3 +17,33 @@ function validateIP()
         fi
         return $stat
 }
+
+
+###((((( highly recommended you change the port SSH server listens on! ))))))###
+TEST_SSH_PORT=22  #(recommended range is 1025-65535)
+ASK_SSH='y'
+#DO NOT USE PORT 30303
+while [ "$ASK_SSH" = "y" ]; do
+  #clear
+  read -p "Would you like to change SSH server to listen on a non-default port? (y/n): " SET_SSH
+  if [ "$SET_SSH" = "y" ]; then
+    read -p "What port should SSH server listen on? (Options: 1024-65535 but not 30303): " TEST_SSH_PORT
+  fi
+  if [ $TEST_SSH_PORT -eq 22 ] || ([ $TEST_SSH_PORT -ne 30303 ] && [ $TEST_SSH_PORT -gt 1023 ] && [ $TEST_SSH_PORT -lt 65536 ]); then
+    ASK_SSH='n'
+    SSHD_PORT=$TEST_SSH_PORT
+  else
+    echo "SSH cannot run on port 30303, nor be on a port less than 1024, nor be on a port higher than 65535.  Try again."
+    TEST_SSH_PORT=22
+  fi
+  echo
+done
+
+#check sshd port
+CHANGESSH="0"
+if [ "$SSHD_PORT" != "22" ]; then
+  CHANGESSH="1"
+fi
+
+echo $SECTION_SEPARATOR
+echo
