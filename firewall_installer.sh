d91d62286345ca58d1138299a5e80987fe3fbb3f
plataplomo <43726571+plataplomo@users.noreply.github.com>
2018-09-30 21:06:52 -0500

    Second chance (#5)
    * Add files via upload
    
    * split into modules
    
    * Delete mn_installer_premium.sh
    
    * Rename mn_installer.sh to mn_installer_premium.sh
    
    * Rename README.md to README1.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update README1.md
    
    * Delete README.md
    
    * Rename README1.md to README.md
    
    * make executable
    
    * Update mn_installer_premium.sh
    
    * Update mn_installer_premium.sh
    
    * Update mn_installer_premium.sh
    
    * Update firewall_installer.sh
    
    * Update firewall_installer.sh
    
    * Update firewall_installer.sh
    
    * Update mn_installer_premium.sh

--- firewall_installer.sh
+++ firewall_installer.sh
@@ -1,6 +1,6 @@
-#/usr/bin/env bash
+#!/bin/bash
 
-SECTION_SEPARATOR="#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#"
+SECTION_SEPARATOR="========================================="
 
 function validateIP()
  {
@@ -18,27 +18,6 @@ function validateIP()
         return $stat
 }
 
-echo $SECTION_SEPARATOR
-
-## https://poseidon.pirl.io/accounts/masternodes-list-private/
-MASTERNODE=""
-while [ "$MASTERNODE" = "" ]; do
-  echo "Copy/Paste in the MN token.  It can be found at https://poseidon.pirl.io/accounts/masternodes-list-private/"
-  read -p 'Enter MN token:' MASTERNODE
-  echo
-done
-
-echo $SECTION_SEPARATOR
-
-## https://poseidon.pirl.io/accounts/settings/
-TOKEN=""
-while [ "$TOKEN" = "" ]; do
-  echo "Copy/Paste in your POSEIDON account's token.  It can be found at https://poseidon.pirl.io/accounts/settings/"
-  read -p 'Enter TOKEN:' TOKEN
-  echo
-done
-
-echo $SECTION_SEPARATOR
 
 ###((((( highly recommended you change the port SSH server listens on! ))))))###
 TEST_SSH_PORT=22  #(recommended range is 1025-65535)
@@ -46,7 +25,7 @@ ASK_SSH='y'
 #DO NOT USE PORT 30303
 while [ "$ASK_SSH" = "y" ]; do
   #clear
-  read -p "Would you like to change SSH server to listen on a non-default port? (y/n): " SET_SSH
+  read -p "Would you like to change SSH server to listen on a non-default port? (y/N): " SET_SSH
   if [ "$SET_SSH" = "y" ]; then
     read -p "What port should SSH server listen on? (Options: 1024-65535 but not 30303): " TEST_SSH_PORT
   fi
@@ -67,6 +46,8 @@ if [ "$SSHD_PORT" != "22" ]; then
 fi
 
 echo $SECTION_SEPARATOR
+echo
+
 
 #your home ip address for the firewall to only allow your ip into ssh
 #if you do not have an ip at home that stays the same, leave the below value
@@ -77,6 +58,7 @@ while [ "$TEST_IP_ADDRESS" = "" ] && [ "$YOURIP" = "" ]; do
   echo "If you wish to limit SSH access to a single IP address enter it here."
   echo "NOTE:  Setting this will secure your server to ONLY allow SSH from this IP address."
   echo "DO NOT set this if you don't have a static IP address to source your SSH sessions to this server from!"
+  echo
   read -p "Enter in your source IP address.  (Leave blank if you wish to allow from any IP.) IP Address: " TEST_IP_ADDRESS
 ### This still doesn't work! ###
 #  if [ $(validateIP $TEST_IP_ADDRESS) -eq 0 ]; then
@@ -99,56 +81,31 @@ while [ "$TEST_IP_ADDRESS" = "" ] && [ "$YOURIP" = "" ]; do
 done
 
 echo $SECTION_SEPARATOR
-
-#username you want the service to run as, if you want it to run as root, leave root
-#if you want it to run as pirl put in pirl. no spaces allowed, and all lower case please.
-#this user will not be used as a login user, so no password will be set.
-read -p "What username should the PIRL Masternode run as? (root, pirl, or leave blank to run as current user): " TEST_RUNAS_USER
-if [ "$TEST_RUNAS_USER" = "" ]; then
-  RUNAS_USER=`logname`
-else
-  RUNAS_USER=$TEST_RUNAS_USER
-fi
-
-#check if username already exists,(if not we will make it later)
-#if so, does it have a valid home dir for chain storage?
-CREATEUSERNAME=0
-getent passwd $RUNAS_USER > /dev/null
-if [ $? -eq 0 ]; then
-    echo "User $RUNAS_USER exists"
-    homedir=$( getent passwd "$RUNAS_USER" | cut -d: -f6 )
-    if [ ! -d $homedir ]; then
-      echo "$RUNAS_USER has no home dir, or its not available. exiting."
-      exit 4
-    fi
- else
-   echo "User $RUNAS_USER not found, will create."
-   CREATEUSERNAME=1
-   sleep 1
-fi
-
-#create the user if needed. just a run as user, not a login user, but they must have a home dir for the chain storage
-if [ "$CREATEUSERNAME" = "1" ]; then
-   getent passwd $RUNAS_USER > /dev/null || useradd -r -m -s /usr/sbin/nologin -c "pirl masternode user" $RUNAS_USER
-fi
-
-#make sure its was created
-getent passwd $RUNAS_USER > /dev/null
-if [ $? -eq 0 ]; then
-    echo "User $RUNAS_USER created"
-    homedir=$( getent passwd "$RUNAS_USER" | cut -d: -f6 )
-    if [ ! -d $homedir ]; then
-      echo "New users home dir created as well @ $homedir"
-    fi
- else
- echo "user $RUNAS_USER not found, tried to create but failed. stopping"
- exit 4
-fi
-
-
-echo $SECTION_SEPARATOR
 echo
+
+#ask if wants to upgrade dist
+ASK_DIST_UPGRADE="y"
+while [ "$ASK_DIST_UPGRADE" = "y" ]; do
+  read -p "Would you like to upgrade Linux distributive to latest version? (y/N): " SET_DIST_UPGRADE
+  if [[ "$SET_DIST_UPGRADE" = "y" || "$SET_DIST_UPGRADE" = "Y" ]]; then
+  	SET_DIST_UPGRADE="y"
+  	ASK_DIST_UPGRADE="n"
+  else
+    if [[ "$SET_DIST_UPGRADE" = "n" || "$SET_DIST_UPGRADE" = "N" || "$SET_DIST_UPGRADE" = "" ]]; then
+    	SET_DIST_UPGRADE="n"
+	ASK_DIST_UPGRADE="n"
+    fi
+  fi
+  echo
+done
+
 echo "Updating/Installing packages.  This will take a few minutes."
+
+#install which because some distributives can come without it
+
+apt install which -y >/dev/null 2>/dev/null
+apt-get install which -y >/dev/null 2>/dev/null
+yum install -y which >/dev/null 2>/dev/null
 
 ############## update packages ###################
 #determine if apt, apt-get or yum
@@ -166,7 +123,9 @@ then
 	name=`lsb_release -sc`
 	echo "deb http://us.archive.ubuntu.com/ubuntu/ $name universe" > /etc/apt/sources.list.d/mn-universe.list
 apt update
-apt full-upgrade -y
+if [ "$SET_DIST_UPGRADE" = "y"]; then
+   apt full-upgrade -y
+fi
 apt install ufw -y
 apt install fail2ban -y
 apt install wget -y
@@ -179,13 +138,15 @@ then
         #get codename
         name=`lsb_release -sc`
         echo "deb http://us.archive.ubuntu.com/ubuntu/ $name universe" > /etc/apt/sources.list.d/mn-universe.list
-apt update
-apt full-upgrade -y
-apt install ufw -y
-apt install fail2ban -y
-apt install wget -y
-apt install setools -y
-apt install policycoreutils-python-utils -y
+apt-get update
+if [ "$SET_DIST_UPGRADE" = "y"]; then
+   apt-get full-upgrade -y
+fi
+apt-get install ufw -y
+apt-get install fail2ban -y
+apt-get install wget -y
+apt-get install setools -y
+apt-get install policycoreutils-python-utils -y
 
 fi
 
@@ -199,73 +160,14 @@ yum install -y wget
 yum install -y setools
 yum install -y policycoreutils-python
 yum install -y policycoreutils 
-yum update -y
+if [ "$SET_DIST_UPGRADE" = "y" ]; then
+   yum update -y
+fi
 fi
 
 
 echo $SECTION_SEPARATOR
 echo
-
-############# grab the node binary and chmod ############################
-###the chain will end up being stored on this users home dir, at /home/username/.pirl/
-
-##make sure its not running if for reason the service is already there, do clean up incase it was run again  for some reason
-echo "Stopping pirlnode, if it is running."
-systemctl stop pirlnode 2>/dev/null 1>/dev/null
-if [ -e /usr/local/bin/pirl-linux-amd6 ]; then
-  echo "Cleaning up previous PIRL installation."
-  rm -f /usr/local/bin/pirl-linux-amd64 2>/dev/null
-fi
-#get pirl node
-echo "downloading latest PIRL Masternode"
-wget -O /usr/local/bin/pirl-linux-amd64 http://release.pirl.io/downloads/masternode/linux/pirl-linux-amd64
-downloadresult=$?
-chmod 0755 /usr/local/bin/pirl-linux-amd64
-chmodresult=$?
-
-#double check download and perms
-if [ "$downloadresult" != "0" ] || [ "$chmodresult" != "0" ]; then
-  echo "error happened downloading the node from http://release.pirl.io/downloads/masternode/linux/pirl-linux-amd64"
-  echo "or trying to chmod it to 0755 at location /usr/local/bin/pirl-linux-amd64"
-  exit 6
-fi
-
-echo $SECTION_SEPARATOR
-echo
-
-############ populate files for systemd service #########
-echo "Create systemd unit file, install, and start."
-echo "[Unit]
-Description=Pirl Master Node
-
-[Service]
-EnvironmentFile=/etc/pirlnode-env
-
-Type=simple
-User=$RUNAS_USER
-Group=$RUNAS_USER
-
-ExecStart=/usr/local/bin/pirl-linux-amd64
-Restart=always
-
-[Install]
-WantedBy=default.target
-">/etc/systemd/system/pirlnode.service
-
-echo "MASTERNODE=\"$MASTERNODE\"
-TOKEN=\"$TOKEN\"
-">/etc/pirlnode-env
-
-###reload in case it was there before, and now could be changed
-systemctl daemon-reload
-
-####enable the node
-systemctl enable pirlnode
-
-###start the node
-systemctl start pirlnode
-
-
 
 ############## update ssh port ###################
 if [ "$CHANGESSH" = "1" ]; then
@@ -341,11 +243,4 @@ ufw status
 
 echo $SECTION_SEPARATOR
 echo
-echo "all done!"
-echo
-echo "commands you can run now:"
-echo "Check firewall status with: 'ufw status'"
-echo "Check PIRL status with: 'systemctl status pirlnode'"
-echo "Watch PIRL system logs with: 'journalctl -f -u pirlnode'"
-
 exit 0
---
+++ mn_installer_premium.sh
@@ -0,0 +1,294 @@
+#!/bin/bash
+
+SECTION_SEPARATOR="========================================="
+ENV_PATH=/etc/pirlnode-env
+DOWNLOAD_LINK_PREMIUM="http://storage.gra1.cloud.ovh.net/v1/AUTH_8f059abdcba74107a430604cf1c257bb/masternode/premium/pirl-v5-masternode-premium-beta"
+DOWNLOAD_LINK_MARLIN="http://storage.gra1.cloud.ovh.net/v1/AUTH_8f059abdcba74107a430604cf1c257bb/masternode/premium/marlin-v5-masternode-premium-beta"
+PREMIUM_PATH=/usr/local/bin/pirl-premium-core
+MARLIN_PATH=/usr/local/bin/pirl-premium-marlin
+
+echo $SECTION_SEPARATOR
+echo
+
+## https://poseidon.pirl.io/accounts/masternodes-list-private/
+MASTERNODE=""
+echo "Copy/Paste in the MN token.  It can be found at https://poseidon.pirl.io/accounts/masternodes-list-private/"
+echo "Or leave it blank if you already have it written in $ENV_PATH and want no change"
+echo
+read -p 'Enter MN token:' MASTERNODE
+echo
+
+if [[ -f $ENV_PATH && "$MASTERNODE" = "" ]]; then
+	echo "Leaving MN token as is"
+	echo
+ else
+ 	if [[ ! -f $ENV_PATH && "$MASTERNODE" = "" ]]; then
+		echo "$ENV_PATH file for tokens doesn't exist"
+	fi
+	echo
+ 	rm -f $ENV_PATH
+	while [ "$MASTERNODE" = "" ]; do
+		echo "Copy/Paste in the MN token.  It can be found at https://poseidon.pirl.io/accounts/masternodes-list-private/"
+		read -p 'Enter MN token:' MASTERNODE
+		echo
+	done
+ fi
+
+echo $SECTION_SEPARATOR
+echo
+
+## https://poseidon.pirl.io/accounts/settings/
+TOKEN=""
+if [ "$MASTERNODE" != "" ]; then
+	while [ "$TOKEN" = "" ]; do
+	  echo "Copy/Paste in your POSEIDON account's token.  It can be found at https://poseidon.pirl.io/accounts/settings/"
+	  echo
+	  read -p 'Enter TOKEN:' TOKEN
+	  echo
+	done
+ else
+	echo "Leaving POSEIDON token as is"
+ fi
+echo
+
+echo $SECTION_SEPARATOR
+echo
+
+#username you want the service to run as, if you want it to run as root, leave root
+#if you want it to run as pirl put in pirl. no spaces allowed, and all lower case please.
+#this user will not be used as a login user, so no password will be set.
+read -p "What username should the PIRL Masternode run as? (root, pirl, or leave blank to run as current user): " TEST_RUNAS_USER
+if [ "$TEST_RUNAS_USER" = "" ]; then
+  RUNAS_USER=`logname`
+else
+  RUNAS_USER=$TEST_RUNAS_USER
+fi
+
+#check if username already exists,(if not we will make it later)
+#if so, does it have a valid home dir for chain storage?
+CREATEUSERNAME=0
+getent passwd $RUNAS_USER > /dev/null
+if [ $? -eq 0 ]; then
+    echo "User $RUNAS_USER exists"
+    homedir=$( getent passwd "$RUNAS_USER" | cut -d: -f6 )
+    if [ ! -d $homedir ]; then
+      echo "$RUNAS_USER has no home dir, or its not available. exiting."
+      exit 4
+    fi
+ else
+   echo "User $RUNAS_USER not found, will create."
+   CREATEUSERNAME=1
+   sleep 1
+fi
+
+#create the user if needed. just a run as user, not a login user, but they must have a home dir for the chain storage
+if [ "$CREATEUSERNAME" = "1" ]; then
+   getent passwd $RUNAS_USER > /dev/null || useradd -r -m -s /usr/sbin/nologin -c "pirl masternode user" $RUNAS_USER
+fi
+
+#make sure its was created
+getent passwd $RUNAS_USER > /dev/null
+if [ $? -eq 0 ]; then
+    echo "User $RUNAS_USER created"
+    homedir=$( getent passwd "$RUNAS_USER" | cut -d: -f6 )
+    if [ ! -d $homedir ]; then
+      echo "New users home dir created as well @ $homedir"
+    fi
+ else
+ echo "user $RUNAS_USER not found, tried to create but failed. stopping"
+ exit 4
+fi
+
+
+echo $SECTION_SEPARATOR
+echo
+
+############# grab the node binary and chmod ############################
+###the chain will end up being stored on this users home dir, at /home/username/.pirl/
+
+##make sure its not running if for reason the service is already there, do clean up incase it was run again  for some reason
+echo "Stopping pirlnode, if it is running."
+systemctl stop pirlnode 2>/dev/null 1>/dev/null
+if [ -e $PREMIUM_PATH ]; then
+  echo "Cleaning up previous PIRL installation."
+  rm -f $PREMIUM_PATH 2>/dev/null
+fi
+#get pirl node
+echo "downloading latest PIRL Masternode"
+wget -O $PREMIUM_PATH $DOWNLOAD_LINK_PREMIUM
+downloadresult=$?
+chmod 0755 $PREMIUM_PATH
+chmodresult=$?
+
+#double check download and perms
+if [ "$downloadresult" != "0" ] || [ "$chmodresult" != "0" ]; then
+  echo "error happened downloading the node from"
+  echo $DOWNLOAD_LINK_PREMIUM
+  echo "or trying to chmod it to 0755 at location"
+  echo $PREMIUM_PATH
+  exit 6
+fi
+
+echo $SECTION_SEPARATOR
+echo
+
+############# grab the marlin-node binary and chmod ############################
+###the chain will end up being stored on this users home dir, at /home/username/.pirl/
+
+##make sure its not running if for reason the service is already there, do clean up incase it was run again  for some reason
+echo "Stopping pirlnode, if it is running."
+systemctl stop pirlmarlin 2>/dev/null 1>/dev/null
+if [ -e $MARLIN_PATH ]; then
+  echo "Cleaning up previous PIRL installation."
+  rm -f $MARLIN_PATH 2>/dev/null
+fi
+#get pirl-marlin node
+echo "downloading latest PIRL Marlin"
+wget -O $MARLIN_PATH $DOWNLOAD_LINK_MARLIN
+downloadresult=$?
+chmod 0755 $MARLIN_PATH
+chmodresult=$?
+
+#double check download and perms
+if [ "$downloadresult" != "0" ] || [ "$chmodresult" != "0" ]; then
+  echo "error happened downloading the node from"
+  echo $DOWNLOAD_LINK_MARLIN
+  echo "or trying to chmod it to 0755 at location"
+  echo $MARLIN_PATH
+  exit 6
+fi
+
+echo $SECTION_SEPARATOR
+echo
+
+############ populate files for systemd service #########
+echo "Create pirl-node systemd unit file, install, and start."
+echo "[Unit]
+Description=Pirl Master Node
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+EnvironmentFile=$ENV_PATH
+
+Type=simple
+User=$RUNAS_USER
+Group=$RUNAS_USER
+RestartSec=30s
+ExecStart=$PREMIUM_PATH --rpc --ws
+Restart=always
+
+[Install]
+WantedBy=default.target
+">/etc/systemd/system/pirlnode.service
+
+if [[ -f $ENV_PATH ]]; then
+	echo "Tokens haven't been changed"
+else
+echo "MASTERNODE=\"$MASTERNODE\"
+TOKEN=\"$TOKEN\"">$ENV_PATH
+echo "Successfully created $ENV_PATH with new tokens"
+fi
+
+###reload in case it was there before, and now could be changed
+systemctl daemon-reload
+
+####enable the node
+systemctl enable pirlnode
+
+###start the node
+systemctl restart pirlnode
+
+
+############ populate files for systemd-marlin service #########
+echo "Create pirl-marlin systemd unit file, install, and start."
+echo "[Unit]
+Description=Pirl Client -- marlin content service
+After=network.target pirlnode.service
+Wants=network.target pirlnode.service
+
+[Service]
+EnvironmentFile=$ENV_PATH
+
+Type=simple
+User=$RUNAS_USER
+Group=$RUNAS_USER
+RestartSec=30s
+ExecStartPre=/bin/sleep 5
+ExecStart=$MARLIN_PATH daemon
+Restart=always
+
+[Install]
+WantedBy=default.target
+">/etc/systemd/system/pirlmarlin.service
+
+if [[ ! -d $homedir/.marlin/ || ! -f $homedir/.marlin/config ]]; then
+	rm -rf $homedir/.marlin/
+	echo "Wait 5 seconds for pirlnode to run before initializing marlin"
+	echo -ne ".\r"
+	sleep 1
+	echo -ne "..\r"
+	sleep 1
+	echo -ne "...\r"
+	sleep 1
+	echo -ne "....\r"
+	sleep 1
+	echo -ne ".....\r"
+	sleep 1
+	echo -ne "\r\033[K"
+	su -c "$MARLIN_PATH init 1>/dev/null" $RUNAS_USER -s /bin/bash
+	chown -R $RUNAS_USER:$RUNAS_USER $homedir/.marlin/
+	
+	if [ -f $homedir/.marlin/config ]; then
+		echo "Pirl marlin successfully initialized"
+ 	else
+  		echo "Something went wrong with initializing marlin folder"
+		echo "Please run '$MARLIN_PATH init' manually after installation"
+	fi
+fi
+
+###reload in case it was there before, and now could be changed
+systemctl daemon-reload
+
+####enable the node
+systemctl enable pirlmarlin
+
+###start the node
+systemctl restart pirlmarlin
+
+
+echo $SECTION_SEPARATOR
+echo
+
+#ask if wants to install firewall and change ssh
+ASK_FIREWALL="y"
+while [ "$ASK_FIREWALL" = "y" ]; do
+  read -p "Would you like to install and configure firewall and change SSH settings? (y/N): " SET_FIREWALL
+  if [[ "$SET_FIREWALL" = "y" || "$SET_FIREWALL" = "Y" ]]; then
+  	SET_FIREWALL="y"
+  	ASK_FIREWALL="n"
+  else
+    if [[ "$SET_FIREWALL" = "n" || "$SET_FIREWALL" = "N" || "$SET_FIREWALL" = "" ]]; then
+    	SET_FIREWALL="n"
+	ASK_FIREWALL="n"
+    fi
+  fi
+  echo
+done
+
+if [ "$SET_FIREWALL" = "y" ]; then
+   bash ./firewall_installer.sh
+fi
+
+echo "All done!"
+echo
+echo "Commands you can run now:"
+echo "Check PIRL-node status with: 'systemctl status pirlnode'"
+echo "Check PIRL-marlin status with: 'systemctl status pirlmarlin'"
+echo "Watch PIRL-node system logs with: 'journalctl -f -u pirlnode'"
+echo "Watch PIRL-marlin system logs with: 'journalctl -f -u pirlmarlin'"
+if [ "$SET_FIREWALL" = "y" ]; then
+   echo "Check firewall status with: 'ufw status'"
+fi
+
+exit 0
